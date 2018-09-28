8087dcef7c36608a2ad5d4e386bcc27c58ddaf05
Lam Ngo <ngobasonlam@gmail.com>
2018-09-28 14:29:31 -0400

    now it should work

--- app/routes/attach_detach_routes.js
+++ app/routes/attach_detach_routes.js
@@ -1,20 +1,24 @@
 module.exports = function(app, childproc) {
 
   app.get('/detach', (req, res) => {
-    var test = childproc(`python /opt/3Gdetach.py ${req.ip}`,
+    var ipStr = `${req.ip}`;
+    var ip = ipStr.substring(ipStr.lastIndexOf(":") + 1);
+    var test = childproc(`python /opt/3Gdetach.py ${ip}`,
               (error, stdout, stderr) => {
                 if (error == null) {
                   res.sendStatus(200);
                 } else {
+                  console.log(error);
                   res.sendStatus(400);
                 }
+
               });
   })
 
   app.get('/attach', (req, res) => {
     var ipStr = `${req.ip}`;
-    console.log(ipStr.substring(ipStr.lastIndexOf(":")));
-    var test = childproc(`python /opt/3Gattach.py ${req.ip}`,
+    var ip = ipStr.substring(ipStr.lastIndexOf(":") + 1);
+    var test = childproc(`python /opt/3Gattach.py ${ip}`,
               (error, stdout, stderr) => {
                 if (error == null) {
                   res.sendStatus(200);
