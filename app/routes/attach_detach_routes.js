4ba9ebe4447286817746fd032e02e68724c0c3bd
Lam Ngo <ngobasonlam@gmail.com>
2018-09-28 14:33:24 -0400

    now with cd

--- app/routes/attach_detach_routes.js
+++ app/routes/attach_detach_routes.js
@@ -3,7 +3,7 @@ module.exports = function(app, childproc) {
   app.get('/detach', (req, res) => {
     var ipStr = `${req.ip}`;
     var ip = ipStr.substring(ipStr.lastIndexOf(":") + 1);
-    var test = childproc(`python /opt/3Gdetach.py ${ip}`,
+    var test = childproc(`cd /opt && python 3Gdetach.py ${ip}`,
               (error, stdout, stderr) => {
                 if (error == null) {
                   res.sendStatus(200);
@@ -18,7 +18,7 @@ module.exports = function(app, childproc) {
   app.get('/attach', (req, res) => {
     var ipStr = `${req.ip}`;
     var ip = ipStr.substring(ipStr.lastIndexOf(":") + 1);
-    var test = childproc(`python /opt/3Gattach.py ${ip}`,
+    var test = childproc(`cd /opt && python 3Gattach.py ${ip}`,
               (error, stdout, stderr) => {
                 if (error == null) {
                   res.sendStatus(200);
