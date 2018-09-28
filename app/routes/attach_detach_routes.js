3422dd4e9afee00470a87071d7b5a946071e84b8
Lam Ngo <ngobasonlam@gmail.com>
2018-09-28 15:14:46 -0400

    now with error checking

--- app/routes/attach_detach_routes.js
+++ app/routes/attach_detach_routes.js
@@ -1,33 +1,53 @@
 module.exports = function(app, childproc) {
+  var attached_list = {}
 
   app.get('/detach', (req, res) => {
     var ipStr = `${req.ip}`;
     var ip = ipStr.substring(ipStr.lastIndexOf(":") + 1);
-    var test = childproc(`cd /opt && python 3Gdetach.py ${ip}`,
-              (error, stdout, stderr) => {
-                if (error == null) {
-                  res.sendStatus(200);
-                } else {
-                  console.log(error);
-                  res.sendStatus(400);
-                }
+    if (attached_list[ip]) {
+      var test = childproc(`cd /opt && python 3Gdetach.py ${ip}`,
+                (error, stdout, stderr) => {
+                  if (error == null) {
+                    res.sendStatus(200);
+                    delete attached_list[ip]
+                  } else {
+                    console.log(error);
+                    res.sendStatus(400);
+                  }
 
-              });
+                });
+    }
+  })
+
+  app.get('/status', (req, res) => {
+    var ipStr = `${req.ip}`;
+    var ip = ipStr.substring(ipStr.lastIndexOf(":") + 1);
+    console.log(`ip address is ${ip}`)
+
+    if (attached_list[ip]) {
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
+    }
   })
 
   app.get('/attach', (req, res) => {
     var ipStr = `${req.ip}`;
     var ip = ipStr.substring(ipStr.lastIndexOf(":") + 1);
     console.log(`ip address is ${ip}`)
-    var test = childproc(`cd /opt && python 3Gattach.py ${ip}`,
-              (error, stdout, stderr) => {
-                if (error == null) {
-                  res.sendStatus(200);
-                } else {
-                  console.log(error);
-                  res.sendStatus(400);
-                }
 
-              });
+    if (!(ip in attached_list)) {
+      var test = childproc(`cd /opt && python 3Gattach.py ${ip}`,
+                (error, stdout, stderr) => {
+                  if (error == null) {
+                    res.sendStatus(200);
+                    attached_list[ip] = true
+                  } else {
+                    console.log(error);
+                    res.sendStatus(400);
+                  }
+
+                });
+    }
   })
 }
