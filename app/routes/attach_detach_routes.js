552d3a17e0722e077e217b08d7638cdf85c561d0
Lam Ngo <ngobasonlam@gmail.com>
2018-09-28 14:19:25 -0400

    this should work

--- app/routes/attach_detach_routes.js
+++ app/routes/attach_detach_routes.js
@@ -2,14 +2,25 @@ module.exports = function(app, childproc) {
   var result = undefined;
 
   app.post('/detach', (req, res) => {
-    console.log(req.body)
-    result = req.body
+    var test = childproc(`python /opt/3Gdetach.py ${req.ip}`,
+              (error, stdout, stderr) => {
+                if (error == null) {
+                  res.sendStatus(200);
+                } else {
+                  res.sendStatus(400);
+                }
+              });
   })
 
   app.get('/attach', (req, res) => {
-    var test = childproc(`echo "abcd"`,
+    var test = childproc(`python /opt/3Gattach.py ${req.ip}`,
               (error, stdout, stderr) => {
-                res.sendStatus(200);
+                if (error == null) {
+                  res.sendStatus(200);
+                } else {
+                  res.sendStatus(400);
+                }
+
               });
   })
 }
