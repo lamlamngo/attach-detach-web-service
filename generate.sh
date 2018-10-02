6140a7ec8eac658b78a730cde180ab7c2ea12383
alef <alef@debian-base>
2018-10-02 13:28:36 -0400

    shell script for cdr

---
+++ generate.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+A=$(sshpass -p alef@123 ssh alef@10.14.32.23 "./latest_folder.sh")
+
+B=$(sshpass -p alef@123 ssh alef@10.14.32.23 "./latest_file.sh")
+
+C=$(sshpass -p alef@123 ssh alef@10.14.32.23 "./second_file.sh")
+
+D="/"
+E=".outp"
+scp alef@10.14.32.23:/home/alef/cdrs/acked/$A$D$B .
+
+echo $B
+if [ -f $B ]; then
+cdrconvert -asn /opt/marben/ASN_1_CGF_Decoder_Dictionary.asn -msg GPRSRecord -i $B -o cdr_output.xml
+sudo python /opt/marben/cdrparse1.py cdr_output.xml $1
+cat /opt/marben/$1$E
+else
+echo "wrong file"
+fi
