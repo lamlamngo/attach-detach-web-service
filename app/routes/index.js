ccdf9e82a86a9a04a0ce6485e3d4cc365c801804
Lam Ngo <ngobasonlam@gmail.com>
2019-04-10 11:53:58 -0400

    add missing files

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,5 @@
 const attach_detach_routes = require('./attach_detach_routes');
-const set_unset_routes = require('./set_unset_routes');
 
-module.exports = function(app, childproc, logger) {
-  attach_detach_routes(app,childproc, logger);
-  set_unset_routes(app);
+module.exports = app => {
+  attach_detach_routes(app);
 };
--- index.js
+++ index.js
@@ -1,35 +1,20 @@
+/**
+ * Third party dependencies
+ */
 const express = require('express');
 const bodyParser = require('body-parser');
-const exec = require('child_process').exec;
-const winston = require('winston');
 
-var alef_results;
-var cloud_results;
+/**
+ * Logger set up
+ */
+var logger = require('./app/utils/log');
 
-var fs = require('fs');
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-      winston.format.timestamp(),
-      winston.format.json()
-  ),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
-  ]
-});
-
-if (process.env.NODE_ENV !== 'production') {
-logger.add(new winston.transports.Console({
-  format: winston.format.simple()
-}));
-}
+/**
+ * express set up
+ */
 
 const app = express();
 app.use(express.static('public'))
-
 const port = 9000;
 
 app.use(bodyParser.urlencoded({
@@ -37,9 +22,9 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-require('./app/routes')(app, exec, logger, {});
-
+require('./app/routes')(app, {});
 
 app.listen(port, () => {
   logger.info('We are live on ' + port);
 })
+
